6d46e9ba8e8af2bf7f5304969de859bfa3427b9c
sairu-sh <tmg.sairush@gmail.com>
2024-01-12 00:08:55 +0545

    get all name and age of profiles after filtering according to age and gender preference of the current user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import userRoutes from "./users";
 import userDetailsRoutes from "./userDetails";
 import interestRoutes from "./interests";
 import updateLocationRoutes from "./updateLocation";
+import getProfileRoutes from "./get-profiles.routes";
 import { auth } from "./../middleware/auth";
 
 const router = Router();
@@ -17,5 +18,7 @@ router.use("/interests", interestRoutes);
 router.use("/create-details", auth, userDetailsRoutes);
 
 router.use("/update-location", auth, updateLocationRoutes);
+
+router.use("/get-profiles", auth, getProfileRoutes);
 
 export default router;
